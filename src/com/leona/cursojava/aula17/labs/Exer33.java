8d69d41d2fed0e3cc3c5a5f1db4ef49fa3aa8f20
Patricia <patyamiga16@hotmail.com>
2021-04-25 17:17:41 -0300

    aula17Exer31

--- src/com/leona/cursojava/aula17/labs/Exer33.java
+++ src/com/leona/cursojava/aula17/labs/Exer33.java
@@ -7,8 +7,13 @@ public class Exer33 {
     
         Scanner scan = new Scanner (System.in);
         
-        System.out.println("");
+        System.out.println("Digite o valor de n");
+        int n = scan.nextInt();
         
+        for (int i=1, j=1; i<=n; i ++, j+=2){
+            
+            System.out.println(i + " / " + j + " + " );
+        }
     }
     
 }
--- src/com/leona/cursojava/aula17/labs/Exer34.java
+++ src/com/leona/cursojava/aula17/labs/Exer34.java
@@ -3,11 +3,22 @@ package com.leona.cursojava.aula17.labs;
 import java.util.Scanner;
 
 public class Exer34 {
- 
-    public static void main (String [] args){
-    
-        Scanner scan = new Scanner (System.in);
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o valor de n");
+        int n = scan.nextInt();
+        double soma = 0;
         
-        System.out.println("");
+        for (int i = 1, j = 1; i <= n; i++, j += 2) {
+
+            System.out.println(i + " / " + j + " + ");
+            
+            soma += i/j;
+        }
+        
+        System.out.println("\nSoma = " + soma );
     }
 }
