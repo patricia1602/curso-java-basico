b738a10542214b432c7e685876522c79b7cb6c7b
Patricia <patyamiga16@hotmail.com>
2021-05-12 10:08:11 -0300

    Aula 19. Exer16

---
+++ src/com/leona/cursojava/aula19/labs/Exer16.java
@@ -0,0 +1,42 @@
+package com.leona.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exer16 {
+
+        public static void main (String [] args){
+        
+            Scanner scan = new Scanner(System.in);
+            
+            int[] vetorA = new int[10];
+            
+            for(int i=0; i<vetorA.length; i++){
+                System.out.print("Entre com o valor da posiçao" +i);
+                vetorA[i] = scan.nextInt();
+             }
+            
+            int somaMenor15 = 0;
+            int somaMaior15 = 0;
+            int qtdMaior15 = 0;
+            int igual15 = 0;
+            for(int i=0; i<vetorA.length; i++){
+                if(vetorA[i] == 15){
+                    igual15 ++;
+                }else if(vetorA[i] < 15){
+                    somaMenor15 += vetorA[i];
+                }else{
+                    qtdMaior15++;
+                    somaMaior15 += vetorA[i];
+                }
+            }
+            System.out.print("Vetor A = ");
+            for(int i=0; i<vetorA.length; i++){
+                System.out.print(vetorA[i] + "");
+            }
+            System.out.println();
+            
+            System.out.println("Qtd numeros ==15: " + igual15);
+            System.out.println("Soma numeros <15: " + somaMenor15);
+            System.out.println("Media numeros > 15: " +(somaMaior15/qtdMaior15));
+        }
+}
