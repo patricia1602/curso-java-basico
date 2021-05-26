d5f952a839863c854b5274fd58a2bd452fe344c9
Patricia <patyamiga16@hotmail.com>
2021-05-26 13:48:49 -0300

    Aula19.Exer

---
+++ src/com/leona/cursojava/aula19/labs/Exer21.java
@@ -0,0 +1,13 @@
+package com.leona.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exer21 {
+
+public static void main (String [] args ){
+
+    Scanner scan = new Scanner(System.in);
+    
+    //N/A
+    }    
+}
