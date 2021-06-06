58cecf0cc149aa5b21ba42b9c3641df2af7e9c25
Patricia <patyamiga16@hotmail.com>
2021-06-06 01:04:35 -0300

    Aula19.Exer30

---
+++ src/com/leona/cursojava/aula19/labs/Exer30.java
@@ -0,0 +1,48 @@
+package com.leona.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exer30 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[20];
+        int[] vetorB = new int[vetorA.length];//pares
+        int[] vetorC = new int[vetorA.length];//impares
+
+        for (int i = 0; i < vetorA.length; i++) {
+
+            System.out.println("Entre com o valor da posiçao A - " + i);
+            vetorA[i] = scan.nextInt();
+        }
+        int posB = 0;
+        int posC = 0;
+
+        for (int i = 0; i < vetorA.length; i++) {
+
+            if (vetorA[i] % 2 == 0) {
+                vetorB[posB] = vetorA[i];
+                posB++;
+            } else {
+                vetorC[posC] = vetorA[i];
+                posC++;
+            }
+        }
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+
+        System.out.print("Vetor B = ");
+        for (int i = 0; i<posB; i++) {
+            System.out.print(vetorB[i] + " ");
+        }
+
+        System.out.print("Vetor C = ");
+        for (int i = 0; i<posC; i++) {
+            System.out.print(vetorC[i] + " ");
+        }
+    }
+}
