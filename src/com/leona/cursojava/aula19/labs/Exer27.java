1ce42218525f3127daae41f017ba5dedc38a4092
Patricia <patyamiga16@hotmail.com>
2021-06-06 00:00:10 -0300

    Aula19.Exer27

---
+++ src/com/leona/cursojava/aula19/labs/Exer27.java
@@ -0,0 +1,44 @@
+package com.leona.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exer27 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+        char[] vetorB = new char[vetorA.length];
+
+        for (int i = 0; i < vetorA.length; i++) {
+
+            System.out.println("Entre com um numero da posiçao = " + i);
+            vetorA[i] = scan.nextInt();
+
+            if (vetorA[i] < 7) {
+                vetorB[i] = 'a';
+            } else if (vetorA[i] == 7) {
+                vetorB[i] = 'b';
+            } else if (vetorA[i] > 7 && vetorA[i] < 10) {
+                vetorB[i] = 'c';
+            } else if (vetorA[i] == 10) {
+                vetorB[i] = 'd';
+            } else if (vetorA[i] > 10) {
+                vetorB[i] = 'e';
+            }
+        }
+
+        System.out.print("VetorA = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("VetorB ");
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.print(vetorB[i] + " ");
+        }
+        System.out.println();
+    }
+}
