0f63af980b7783648877fc517d9fae3756cce956
Patricia <patyamiga16@hotmail.com>
2021-06-13 19:36:18 -0300

    Aula20

--- src/com/leona/cursojava/aula20/Matrizes.java
+++ src/com/leona/cursojava/aula20/Matrizes.java
@@ -1,10 +1,10 @@
 package com.leona.cursojava.aula20;
 
 public class Matrizes {
-    
+
     public static void main(String[] args) {
 
-        double[][] notasAlunos = new double[30][4];
+        double[][] notasAlunos = new double[3][4];
 
         notasAlunos[0][0] = 10;
         notasAlunos[0][1] = 7;
@@ -21,5 +21,52 @@ public class Matrizes {
         notasAlunos[2][2] = 10;
         notasAlunos[2][3] = 7;
 
+        for (int i = 0; i < notasAlunos.length; i++) {
+            //System.out.print(notasAlunos[i] + " ");
+            for (int j = 0; j < notasAlunos[i].length; j++) {
+                System.out.print(notasAlunos[i][j] + " - ");
+
+            }
+            System.out.println();
+        }
+        notasAlunos[1][3] = 8;
+
+        System.out.println();
+
+        for (int i = 0; i < notasAlunos.length; i++) {
+
+            for (int j = 0; j < notasAlunos[i].length; j++) {
+                System.out.print(notasAlunos[i][j] + " - ");
+
+            }
+            System.out.println();
+        }
+        System.out.println("Calculando a media de cada aluno");
+
+        double soma;
+        for (int i = 0; i < notasAlunos.length; i++) {
+
+            soma = 0;
+
+            for (int j = 0; j < notasAlunos[i].length; j++) {
+                soma += notasAlunos[i][j];
+            }
+            System.out.println("Media do aluno" + i + " e = " + (soma / 4));
+        }
+
+        double[] notasAlunos1 = {7, 8, 9, 10};
+
+        double[][] notasAlunos2 = {{7, 8, 9, 10}, {8, 6, 7, 10}};
+
+        System.out.println("Output da matriz notasAlunos2");
+
+        for (int i = 0; i < notasAlunos2.length; i++) {
+
+            for (int j = 0; j < notasAlunos2[i].length; j++) {
+                System.out.print(notasAlunos2[i][j] + " - ");
+
+            }
+            System.out.println();
+        }
     }
-}      
+}
