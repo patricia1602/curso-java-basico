02e24ebe35c46cb65691855b2718cd52e7a2579e
Patricia <patyamiga16@hotmail.com>
2021-06-15 18:10:17 -0300

    Aula20.Exer01

--- src/com/leona/cursojava/aula20/labs/Exer01.java
+++ src/com/leona/cursojava/aula20/labs/Exer01.java
@@ -30,7 +30,7 @@ public class Exer01 {
         }
         for (int i = 0; i < numerosAleatorios.length; i++) {
             for (int j = 0; j < numerosAleatorios[i].length; j++) {
-                System.out.println(numerosAleatorios[i][j] + " ");
+                System.out.print(numerosAleatorios[i][j] + " ");
             }
             System.out.println();
         }
