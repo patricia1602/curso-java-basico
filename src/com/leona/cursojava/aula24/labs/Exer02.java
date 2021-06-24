767fc356b4460c7575a9b3e305b4238af1fc2fbe
Patricia <patyamiga16@hotmail.com>
2021-06-24 14:17:21 -0300

    Aula24.Livro

---
+++ src/com/leona/cursojava/aula24/labs/Exer02.java
@@ -0,0 +1,15 @@
+package com.leona.cursojava.aula24.labs;
+
+public class Exer02 {
+
+    public static void main(String[] args) {
+
+        Livro livro = new Livro();
+        livro.nome = "Mastering ExtJS";
+        livro.autor = "Loaine Groner";
+        livro.anoLançamento = 2015;
+
+        System.out.println("Nome do livro = " + livro.nome);
+
+    }
+}
---
+++ src/com/leona/cursojava/aula24/labs/Livro.java
@@ -0,0 +1,11 @@
+package com.leona.cursojava.aula24.labs;
+
+public class Livro {
+
+    String nome;
+    String autor;
+    int qtdPaginas;
+    int anoLançamento;
+    String isbn;
+
+}
