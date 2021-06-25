b3758a539a52b60fb5a37bdf70599b73c6212461
Patricia <patyamiga16@hotmail.com>
2021-06-25 10:35:49 -0300

    Aula24.Contato

---
+++ src/com/leona/cursojava/aula24/labs/Contato.java
@@ -0,0 +1,9 @@
+package com.leona.cursojava.aula24.labs;
+
+public class Contato {
+
+    String nome;
+    String email;
+    String endereço;
+    String [] telefones;
+    }
---
+++ src/com/leona/cursojava/aula24/labs/Exer06.java
@@ -0,0 +1,18 @@
+package com.leona.cursojava.aula24.labs;
+
+public class Exer06 {
+
+    public static void main(String[] args) {
+
+        Contato contato1 = new Contato();
+        contato1.nome = "Joao";
+        contato1.endereço = "Av Paulista, 1000";
+        contato1.email = "joao@email.com";
+
+        contato1.telefones = new String[5];
+        contato1.telefones[0] = "99999-9999";
+        contato1.telefones[1] = "99999-9998";
+        contato1.telefones[2] = "99999-9997";
+
+    }
+}
