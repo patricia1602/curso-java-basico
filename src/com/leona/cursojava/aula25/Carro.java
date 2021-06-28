ee52ebbd14963dde8d21c7536205ec91d489f502
Patricia <patyamiga16@hotmail.com>
2021-06-28 10:41:24 -0300

    Aula25

---
+++ src/com/leona/cursojava/aula25/Carro.java
@@ -0,0 +1,14 @@
+package com.leona.cursojava.aula25;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+    void exibirAutonomia() {
+        System.out.println("A autonomia do carro e:" + capCombustivel * consumoCombustivel + "Km");
+    }
+}
