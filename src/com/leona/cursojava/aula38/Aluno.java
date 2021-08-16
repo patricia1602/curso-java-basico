fccdf7162c8da07a93e5f49bdb58915f058b46c6
Patricia <patyamiga16@hotmail.com>
2021-08-16 10:31:25 -0300

    Aula38

---
+++ src/com/leona/cursojava/aula38/Aluno.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leona.cursojava.aula37;
+
+/**
+ *
+ * @author leona
+ */
+public class Aluno extends Pessoa {
+
+    private String curso;
+    private double[] notas;
+
+    /**
+     * @return the curso
+     */
+    public String getCurso() {
+        return curso;
+    }
+
+    /**
+     * @param curso the curso to set
+     */
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    /**
+     * @return the notas
+     */
+    public double[] getNotas() {
+        return notas;
+    }
+
+    /**
+     * @param notas the notas to set
+     */
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia() {
+        return 0;
+    }
+
+    public boolean verificarAprovado() {
+        return true;
+    }
+    
+}
---
+++ src/com/leona/cursojava/aula38/Teste.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leona.cursojava.aula38;
+
+/**
+ *
+ * @author leona
+ */
+public class Teste {
+    
+}
