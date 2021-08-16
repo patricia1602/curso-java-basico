37b5b4b5f3dbfc2af685aad2b4cb8ed736308d6a
Patricia <patyamiga16@hotmail.com>
2021-08-16 10:01:49 -0300

    Aula37

---
+++ src/com/leona/cursojava/aula37/Professor.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leona.cursojava.aula37;
+
+/**
+ *
+ * @author leona
+ */
+public class Professor extends Pessoa {
+
+    private double salario;
+    private String nomeCurso;
+
+    /**
+     * @return the salario
+     */
+    public double getSalario() {
+        return salario;
+    }
+
+    /**
+     * @param salario the salario to set
+     */
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    /**
+     * @return the nomeCurso
+     */
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    /**
+     * @param nomeCurso the nomeCurso to set
+     */
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public double calcularSalarioLiquido() {
+        return 0;
+    }
+
+}
