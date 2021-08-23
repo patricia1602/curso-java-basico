c91a94069127675cfbee7cd7e074ad54911dfc79
Patricia <patyamiga16@hotmail.com>
2021-08-23 09:57:41 -0300

    Aula42

---
+++ src/com/leona/cursojava/aula42/Contantes.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leona.cursojava.aula42;
+
+/**
+ *
+ * @author leona
+ */
+public final class Contantes {
+    
+    public static final String url_BLOG = "http://loiane.com";
+    public static final String CURSO_COMPLETO = "http://loiane.training";
+            
+    //private String urBlog;        
+}
---
+++ src/com/leona/cursojava/aula42/Teste.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leona.cursojava.aula42;
+
+/**
+ *
+ * @author leona
+ */
+public class Teste {
+ 
+    public static void main(String[] args) {
+        
+        Pessoa pessoa = new Pessoa();
+        //Pessoa aluno = new Aluno ();
+        //Pessoa professor = new Professor();
+        Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+        
+        //exemplo 02
+        System.out.println(Contantes.url_BLOG);
+       // Contantes.url_BLOG = "sdjskjdhfsdf";
+        System.out.println(Contantes.url_BLOG);
+        
+        final int total = 1;
+        //total = total = 1;
+        System.out.println(total);
+    }
+ 
+}
