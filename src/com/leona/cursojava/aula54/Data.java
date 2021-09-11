f7db2c872d082853f24952789799e521c2be021f
Patricia <patyamiga16@hotmail.com>
2021-09-11 17:54:47 -0300

    Aula54

---
+++ src/com/leona/cursojava/aula54/Data.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leona.cursojava.aula54;
+
+/**
+ *
+ * @author leona
+ */
+public class Data {
+    
+    private int dia;
+    private int mes;
+    private int ano;
+    private DiaSemana diaSemana;
+
+    /**
+     * @return the dia
+     */
+    public int getDia() {
+        return dia;
+    }
+
+    /**
+     * @param dia the dia to set
+     */
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    /**
+     * @return the mes
+     */
+    public int getMes() {
+        return mes;
+    }
+
+    /**
+     * @param mes the mes to set
+     */
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    /**
+     * @return the ano
+     */
+    public int getAno() {
+        return ano;
+    }
+
+    /**
+     * @param ano the ano to set
+     */
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    /**
+     * @return the diaSemana
+     */
+    public DiaSemana getDiaSemana() {
+        return diaSemana;
+    }
+
+    /**
+     * @param diaSemana the diaSemana to set
+     */
+    public void setDiaSemana(DiaSemana diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+    
+}
