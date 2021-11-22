8bb36ea2badcab9de4b4694c19ea3ff4eafb461a
Patricia <patyamiga16@hotmail.com>
2021-11-22 09:47:11 -0300

    aula71

---
+++ src/com/leona/cursojava/aula71/Calculadora.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leona.cursojava.aula71;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author leona
+ */
+public class Calculadora {
+
+    private int soma;
+
+    public synchronized int somaArray(int[] array) {
+
+        soma = 0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            soma += array[i];
+
+            System.out.println("Executando a soma " + Thread.currentThread().getName()
+                    + " somando o valor " + array[i] + " com total de "  + soma);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return soma;
+    }
+}
---
+++ src/com/leona/cursojava/aula71/MinhaThreadSoma.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leona.cursojava.aula71;
+
+/**
+ *
+ * @author leona
+ */
+public class MinhaThreadSoma implements Runnable {
+
+    private String nome;
+    private int[] nums;
+    private static Calculadora calc = new Calculadora();
+
+    public MinhaThreadSoma(String nome, int[] nums) {
+        this.nome = nome;
+        this.nums = nums;
+        new Thread(this, nome).start();
+        //Thread t = new Thread(this, nome);
+        //t.start();
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println(this.nome + " iniciada");
+
+        int soma = calc.somaArray(this.nums);
+
+        System.out.println("Resultado da soma para thread" + this.nome + "é" + soma);
+
+        System.out.println(this.nome + " terminada");
+    }
+}
