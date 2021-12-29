4e1faf05f0cfdda553e36549a07fc18ba56e7ba6
Patricia <patyamiga16@hotmail.com>
2021-12-29 12:34:47 -0300

    aula72

---
+++ src/com/leona/cursojava/aula72/TiqueTaque.java
@@ -0,0 +1,52 @@
+package com.leona.cursojava.aula72;
+
+public class TiqueTaque {
+
+    boolean tique;
+
+    synchronized void tique(boolean estaExecutando) {
+
+        if (!estaExecutando) {
+            tique = true;
+            notify();
+            return;
+        }
+
+        System.out.println("Tique");
+
+        tique = true;
+
+        notify();
+
+        try {
+            while (tique) {
+
+                wait();
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+    synchronized void taque(boolean estaExecutando) {
+
+        if (!estaExecutando) {
+            tique = false;
+            notify();
+            return;
+        }
+
+        System.out.println("Taque");
+
+        tique = false;
+
+        notify();
+
+        try {
+            while (!tique) {
+
+                wait();
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+}
